594dc5f72ab62790a88eb3bff51d24048f5bf6bc
Reid <reidsotkindevelopment@gmail.com>
2021-11-17 14:29:25 -0600

    main

---
+++ ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  animalkingdomm
+//
+//  Created by PMStudent on 11/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var isWalktrhoughViewShowing = true
+
+    var body: some View {
+       
+        Group {
+            if isWalktrhoughViewShowing {
+               WalkthroughView(isWalktrhoughViewShowing:
+                                $isWalktrhoughViewShowing)
+            } else {
+FirstQuestionContentView()
+                
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignInView()
+    }
+}
---
+++ animalkingdommApp.swift
@@ -0,0 +1,17 @@
+//
+//  animalkingdommApp.swift
+//  animalkingdomm
+//
+//  Created by PMStudent on 11/9/21.
+//
+
+import SwiftUI
+
+@main
+struct animalkingdommApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
